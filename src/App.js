add6dabeb7ab2254dda0d398ff4c20df2cf2c93c
Phuc Tran <tquangphuc@tma.com.vn>
2023-03-07 14:11:26 +0700

    update file App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React, { Component } from "react";
 import ServiceBus from './ServiceBus';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Webpack</p>
-        <ServiceBus></ServiceBus>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Webpack</p>
+          <ServiceBus></ServiceBus>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
